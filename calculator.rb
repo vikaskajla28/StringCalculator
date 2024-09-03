dfb77b43dfe60c87403333b028112b1536e9ee4d
Vikas Kajla <vikaskajla28@gmail.com>
2024-09-03 18:37:51 +0530

    test(newline_present_instead_of_comma): SUCCESS

--- calculator.rb
+++ calculator.rb
@@ -5,6 +5,7 @@ class Calculator
   
   def add
     return 0 if @numbers == ""
+    @numbers.gsub!("\n", ",") #replace newline with comma if present
     return @numbers.split(",").map{|num| num.to_i}.sum
   end
 end
