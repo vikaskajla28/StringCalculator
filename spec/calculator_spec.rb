f14f6852c34127908edffcb89cbeef9d5144a452
Vikas Kajla <vikaskajla28@gmail.com>
2024-09-03 18:28:54 +0530

    test(empty_string_test): FAILURE

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,4 +4,9 @@ require '../calculator.rb'
 describe Calculator do 
   it "performs first test" do 
   end
+  
+  it "should return 0 when empty string passed" do
+    calculator = Calculator.new("")
+    expect(calculator.add).to eq 0
+  end
 end
