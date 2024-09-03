a3ec46bd2b39d51946f9b2748f605855a385d646
Vikas Kajla <vikaskajla28@gmail.com>
2024-09-03 18:54:23 +0530

    test(negative_numbers_not_allowed): SUCCESS

--- calculator.rb
+++ calculator.rb
@@ -6,12 +6,23 @@ class Calculator
   def add
     return 0 if @numbers == ""
     delimiter = get_delimiter
-    return @numbers.split(delimiter).map{|num| num.to_i}.sum
+    return get_sum(delimiter)
+  end
+  
+  def get_sum(delimiter)
+    negative_numbers = []
+    nums = @numbers.split(delimiter).map do |str|
+      num = str.to_i
+      negative_numbers.push(num) if num < 0
+      num  
+    end
+    raise "negative numbers not allowed: #{negative_numbers.join(",")}" if negative_numbers.size > 0
+    return nums.sum
   end
   
   def get_delimiter
     delimiter = ","
-    if @numbers[0,2] == "//"
+    if @numbers[0,2] == "//" # custom delimiter defined after //
       delimiter = @numbers[2,1] 
       @numbers = @numbers.split("\n")[1] # get the string after the delimiter definition
     else
