51810a8a09a72b289dff6c962366f3075d088ed6
Vikas Kajla <vikaskajla28@gmail.com>
2024-09-03 18:44:01 +0530

    test(custom_delimiter): SUCCESS

--- calculator.rb
+++ calculator.rb
@@ -5,7 +5,18 @@ class Calculator
   
   def add
     return 0 if @numbers == ""
-    @numbers.gsub!("\n", ",") #replace newline with comma if present
-    return @numbers.split(",").map{|num| num.to_i}.sum
+    delimiter = get_delimiter
+    return @numbers.split(delimiter).map{|num| num.to_i}.sum
+  end
+  
+  def get_delimiter
+    delimiter = ","
+    if @numbers[0,2] == "//"
+      delimiter = @numbers[2,1] 
+      @numbers = @numbers.split("\n")[1] # get the string after the delimiter definition
+    else
+      @numbers.gsub!("\n", ",") #replace newline with comma if present
+    end
+    return delimiter
   end
 end
